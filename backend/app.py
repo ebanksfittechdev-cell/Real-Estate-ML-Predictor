31bf9957b8554aebae0be136a9b1bb0d87c4b6f8
Ethan Adragna <ebanksfittechdev@gmail.com>
2026-09-02 07:35:36 -0400

    Set SQLALCHEMY_DATABASE_URI conditionally
    Modify database URI based on Vercel environment variable.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
 from models import db
 from predict import predict_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+
+if os.environ.get('VERCEL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/app.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 CORS(app)
@@ -13,7 +20,5 @@ db.init_app(app)
 app.register_blueprint(predict_bp)
 
 with app.app_context():
-    db.create_all()   # creates app.db and the table on first run, if not already there
+    db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
